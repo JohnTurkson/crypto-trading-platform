f17e3fbc2e93d1d1853f8a3fa67ea5a763a4c70a
John Turkson <git@johnturkson.com>
2021-06-10 00:22:48 -0700

    Add signup prototype

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,23 +4,35 @@ import {
     Home,
     Profile
 } from "./pages"
+import SignUpForm, { SignUpInformation } from "./pages/SignUpForm"
+import { useState } from "react"
+
+
 
 function App() {
+    const [signUpInformation, setSignUpInformation] = useState({
+        name: "",
+        email: "",
+        password: "",
+    })
+    
     return (
-        <Router>
-            <Switch>
-                <Route path="/profile">
-                    <Page>
-                        <Profile />
-                    </Page>
-                </Route>
-                <Route path="/">
-                    <Page>
-                        <Home />
-                    </Page>
-                </Route>
-            </Switch>
-        </Router>
+        <SignUpForm information={signUpInformation} onInformationChange={setSignUpInformation}/>
+        
+        // <Router>
+        //     <Switch>
+        //         <Route path="/profile">
+        //             <Page>
+        //                 <Profile />
+        //             </Page>
+        //         </Route>
+        //         <Route path="/">
+        //             <Page>
+        //                 <Home />
+        //             </Page>
+        //         </Route>
+        //     </Switch>
+        // </Router>
     )
 }
 
--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,15 +1,71 @@
-export default function SignUp() {
-    return <form>
-        <div>
-            <label id="sign-up--email-field" htmlFor="sign-up--email-input">Email</label>
-            <input id="sign-up--email-input" type="text"/>
-        </div>
-        <div>
-            <label id="sign-up--password-field" htmlFor="sign-up--password-input">Password</label>
-            <input id="sign-up--password-input" type="text"/>
-        </div>
-        <div>
-            <input color="primary" type="submit" value="Sign Up"/>
-        </div>
-    </form>
+import React, { Dispatch, FormEvent, SetStateAction } from "react"
+import { Button, Container, makeStyles, TextField, Typography } from "@material-ui/core"
+
+export interface SignUpInformation {
+    name: string
+    email: string
+    password: string
+}
+
+interface SignUpFormProps {
+    information: SignUpInformation
+    onInformationChange: Dispatch<SetStateAction<SignUpInformation>>
+    onSubmit: (event: FormEvent) => void
+}
+
+const useStyles = makeStyles(theme => ({
+    container: {
+        marginTop: theme.spacing(8),
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center"
+    },
+    form: {
+        marginTop: theme.spacing(1)
+    },
+    button: {
+        marginTop: theme.spacing(2)
+    }
+}))
+
+export default function SignUp(props: SignUpFormProps) {
+    console.log(props.information.name)
+
+    const classes = useStyles()
+
+    return (
+        <Container className={classes.container}>
+            <Typography component="h1" variant="h5">
+                Sign Up
+            </Typography>
+            <form className={classes.form}>
+                <TextField
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                    label="Name"
+                />
+                <TextField
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                    label="Email"
+                />
+                <TextField
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                    type="password"
+                    label="Password"
+                />
+                <Button
+                    className={classes.button}
+                    variant="contained"
+                    fullWidth
+                    color="primary">
+                    {"Sign Up"}
+                </Button>
+            </form>
+        </Container>
+    )
 }
