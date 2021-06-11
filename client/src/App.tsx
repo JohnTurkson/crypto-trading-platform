d4aff7bc55e9a7a29f2b266b15287e97e85b3c94
John Turkson <git@johnturkson.com>
2021-06-10 20:35:29 -0700

    Revert App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,35 +4,23 @@ import {
     Home,
     Profile
 } from "./pages"
-import SignUpForm, { SignUpInformation } from "./pages/SignUpForm"
-import { useState } from "react"
-
-
 
 function App() {
-    const [signUpInformation, setSignUpInformation] = useState({
-        name: "",
-        email: "",
-        password: "",
-    })
-    
     return (
-        <SignUpForm information={signUpInformation} onInformationChange={setSignUpInformation}/>
-        
-        // <Router>
-        //     <Switch>
-        //         <Route path="/profile">
-        //             <Page>
-        //                 <Profile />
-        //             </Page>
-        //         </Route>
-        //         <Route path="/">
-        //             <Page>
-        //                 <Home />
-        //             </Page>
-        //         </Route>
-        //     </Switch>
-        // </Router>
+        <Router>
+            <Switch>
+                <Route path="/profile">
+                    <Page>
+                        <Profile />
+                    </Page>
+                </Route>
+                <Route path="/">
+                    <Page>
+                        <Home />
+                    </Page>
+                </Route>
+            </Switch>
+        </Router>
     )
 }
 
