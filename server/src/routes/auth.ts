a96d5f1bead21e06438045ce3afdf9cace18fd96
andykwan1124 <woody.andygrant@gmail.com>
2021-06-23 21:53:12 -0700

    add login endpoint

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -15,4 +15,19 @@ export default (app: express.Application) => {
             res.status(400).json({ message: err.message })
         }
     })
+
+    app.post("/login", async (req, res) => {
+        const { email, password } = req.body
+
+        try {
+            const user = await UserModel.findOne({ email })
+
+            if (!user) throw new Error("User does not exist")
+            if (user.password != password) throw new Error("Wrong password")
+            
+            user.sendToken(res)
+        } catch (err) {
+            res.status(400).json({ message: err.message })
+        }
+    })
 }
