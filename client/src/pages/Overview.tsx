c414a0483217386a44bd160fe523039cf786641f
ryanliu18 <ryanliu418@gmail.com>
2021-06-26 00:51:47 -0700

    Merge branch 'main' into coinList
    # Conflicts:
    #	client/package-lock.json
    #	client/package.json
    #	client/src/App.tsx
    #	client/src/components/Navbar.tsx
    #	client/src/components/Portfolio.tsx
    #	client/src/pages/Overview.tsx

--- client/src/pages/Overview.tsx
+++ client/src/pages/Overview.tsx
@@ -1,7 +1,4 @@
 import CoinList from "../containers/CoinList"
-import React from "react";
-import {SignIn} from "../components/SignIn";
-import Coin from "../components/Coin";
 
 // TODO: add more components to home page
 export default () => {
