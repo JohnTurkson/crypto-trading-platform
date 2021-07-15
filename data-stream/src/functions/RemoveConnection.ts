cb965f86dd011e9213d3fe54f118ace3789dd084
John Turkson <git@johnturkson.com>
2021-07-15 02:35:19 -0700

    Update lambda handlers

--- data-stream/src/functions/RemoveConnection.ts
+++ data-stream/src/functions/RemoveConnection.ts
@@ -1,5 +1,5 @@
 import { DeleteItemCommand } from "@aws-sdk/client-dynamodb"
-import { dynamoDBClient, mongoClient } from "./Clients"
+import { dynamoDBClient } from "./Clients"
 
 export async function handler(event: any) {
     const removeConnectionCommand = new DeleteItemCommand({
@@ -10,17 +10,6 @@ export async function handler(event: any) {
     })
 
     await dynamoDBClient.send(removeConnectionCommand)
-    await mongoClient.db("CryptoDataStream").collection("Connections")
-        .deleteOne({"connectionId": event.requestContext.connectionId})
-
-    // await databaseClient.send(addConnectionCommand)
-    //
-    // const commandInput: PostToConnectionCommandInput = {
-    //     ConnectionId: "",
-    //     Data: new TextEncoder().encode("Remove Connection"),
-    // }
-    // const command = new PostToConnectionCommand(commandInput)
-    // await client.send(command)
 
     return {
         statusCode: 200,
