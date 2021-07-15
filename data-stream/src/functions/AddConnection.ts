19f81143ce4610bc88b4a52239d21b8cd86bc47b
John Turkson <git@johnturkson.com>
2021-07-15 02:35:19 -0700

    Update lambda handlers

--- data-stream/src/functions/AddConnection.ts
+++ data-stream/src/functions/AddConnection.ts
@@ -1,8 +1,5 @@
 import { PutItemCommand } from "@aws-sdk/client-dynamodb"
-import { dynamoDBClient, mongoClient } from "./Clients"
-import { MongoClient } from "mongodb"
-
-const a = new MongoClient('a')
+import { dynamoDBClient } from "./Clients"
 
 export async function handler(event: any) {
     const addConnectionCommand = new PutItemCommand({
@@ -11,18 +8,9 @@ export async function handler(event: any) {
             "connectionId": {S: event.requestContext.connectionId}
         }
     })
+    
     await dynamoDBClient.send(addConnectionCommand)
     
-    await mongoClient.db("CryptoDataStream").collection("Connections")
-        .insertOne({"connectionId": event.requestContext.connectionId})
-
-    // const commandInput: PostToConnectionCommandInput = {
-    //     ConnectionId: "",
-    //     Data: new TextEncoder().encode("AddConnection"),
-    // }
-    // const command = new PostToConnectionCommand(commandInput)
-    // await client.send(command)
-
     return {
         statusCode: 200,
         body: JSON.stringify(event)
--- data-stream/src/functions/Clients.ts
+++ data-stream/src/functions/Clients.ts
@@ -1,14 +1,11 @@
-import { MongoClient } from "mongodb"
 import { ApiGatewayManagementApiClient } from "@aws-sdk/client-apigatewaymanagementapi"
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb"
 
 export const apiGatewayManagementApiClient = new ApiGatewayManagementApiClient({
     region: "us-west-2",
-    endpoint: process.env.DATA_STREAM_ENDPOINT!
+    endpoint: process.env.DATA_STREAM_ENDPOINT
 })
 
 export const dynamoDBClient = new DynamoDBClient({
     region: "us-west-2"
 })
-
-export const mongoClient = new MongoClient(process.env.MONGO_URL!)
