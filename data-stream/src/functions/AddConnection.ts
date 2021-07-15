75dbb2af206491267a1725152d7e655014558d3b
John Turkson <git@johnturkson.com>
2021-07-14 19:26:11 -0700

    Add data-stream lambda functions

---
+++ data-stream/src/functions/AddConnection.ts
@@ -0,0 +1,28 @@
+import {
+    ApiGatewayManagementApiClient,
+    ApiGatewayManagementApiClientConfig,
+    PostToConnectionCommand,
+    PostToConnectionCommandInput
+} from "@aws-sdk/client-apigatewaymanagementapi"
+import { TextEncoder } from "util"
+
+const config: ApiGatewayManagementApiClientConfig = {
+    region: "us-west-2",
+    endpoint: process.env.DATA_STREAM_ENDPOINT
+}
+const client = new ApiGatewayManagementApiClient(config)
+
+export async function handler(event: any) {
+    const a = process.env.DATA_STREAM_ENDPOINT
+    const commandInput: PostToConnectionCommandInput = {
+        ConnectionId: "...=",
+        Data: new TextEncoder().encode("AddConnection"),
+    }
+    const command = new PostToConnectionCommand(commandInput)
+    await client.send(command)
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(event)
+    }
+}
