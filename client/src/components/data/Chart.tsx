0801168d28b57a2a07b76c1d3a1b42df0c679232
ryanliu18 <ryanliu418@gmail.com>
2021-07-16 22:56:04 -0700

    tidy up charts

--- client/src/components/data/Chart.tsx
+++ client/src/components/data/Chart.tsx
@@ -20,10 +20,6 @@ export function Chart(props: ChartProps) {
             offset: 20,
 
             labels: {
-                // formatter: function () {
-                //    return numberFormat.format(this.value)
-                //}
-                //,
                 x: -15,
                 style: {
                     "color": "#000", "position": "absolute"
@@ -36,9 +32,6 @@ export function Chart(props: ChartProps) {
         ],
         tooltip: {
             shared: true,
-            //formatter: function () {
-            //    return numberFormat.format(this.y, 0) +  '</b><br/>' + moment(this.x).format('MMMM Do YYYY, h:mm')
-            //}
         },
         plotOptions: {
             series: {
@@ -110,12 +103,13 @@ export function Chart(props: ChartProps) {
     useEffect(() => {
         connection.onmessage = message => {
             //console.log(message)
-            let currData = data;
+            //let currData = data;
             let json = JSON.parse(message.data)
-            currData.push([json["time"], parseFloat(json["price"])])
+            //currData.push([json["time"], parseFloat(json["price"])])
             //console.log(currData)
-            setData([data,currData])
+            //setData([data,currData])
             //setData(currData)
+            setData([data, [json["time"], parseFloat(json["price"])] ])
 
             let newConfig = config
             newConfig.series[0].data = data;
@@ -124,102 +118,6 @@ export function Chart(props: ChartProps) {
 
         }
     })
-
-
-     */
-
-    /*
-    let configPrice = {
-
-        yAxis: [{
-            offset: 20,
-
-            labels: {
-                // formatter: function () {
-                //    return numberFormat.format(this.value)
-                //}
-                //,
-                x: -15,
-                style: {
-                    "color": "#000", "position": "absolute"
-
-                },
-                align: "left"
-            },
-        },
-
-        ],
-        tooltip: {
-            shared: true,
-            //formatter: function () {
-            //    return numberFormat.format(this.y, 0) +  '</b><br/>' + moment(this.x).format('MMMM Do YYYY, h:mm')
-            //}
-        },
-        plotOptions: {
-            series: {
-                showInNavigator: true,
-                gapSize: 6,
-
-            }
-        },
-
-        //rangeSelector: {
-        //    selected: 1
-        //},
-        title: {
-            text: props.name
-        },
-        chart: {
-            height: 600,
-        },
-
-        credits: {
-            enabled: false
-        },
-
-        legend: {
-            enabled: true
-        },
-        xAxis: {
-            type: "date",
-        },
-        rangeSelector: {
-            buttons: [{
-                type: "day",
-                count: 1,
-                text: "1d",
-            }, {
-                type: "day",
-                count: 7,
-                text: "7d"
-            }, {
-                type: "month",
-                count: 1,
-                text: "1m"
-            }, {
-                type: "month",
-                count: 3,
-                text: "3m"
-            },
-                {
-                    type: "all",
-                    text: "All"
-                }],
-            selected: 4
-        },
-        series: [{
-            name: "Price",
-            type: "spline",
-
-            //data: priceData,
-            //data: data,
-            tooltip: {
-                valueDecimals: 2
-            },
-
-        }
-        ]
-    }
 
      */
 
