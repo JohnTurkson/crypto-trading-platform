ec4a928893680f94f76e12db27b2031d2d7f9306
John Turkson <git@johnturkson.com>
2021-07-16 21:25:52 -0700

    Add default route

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,6 +8,11 @@ const port = process.env.CRYPTO_TRADING_PLATFORM_PORT!
 
 routesHandler(app)
 
+app.get("/", async (request, response) => {
+    console.log(request)
+    response.send("Hello World").end()
+})
+
 app.listen(port, async () => {
     return console.log(`server is listening on ${port}`)
 })
